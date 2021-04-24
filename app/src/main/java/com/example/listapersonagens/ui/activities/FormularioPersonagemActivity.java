ae22dd72554c0e19e870569651163217f5f969ee
Gabriel <gleandro0795@gmail.com>
2021-04-24 14:36:49 -0300

    Add the icon check, mask in the inputs and update the comments

--- app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listapersonagens/ui/activities/FormularioPersonagemActivity.java
@@ -2,15 +2,20 @@ package com.example.listapersonagens.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.listapersonagens.R;
 import com.example.listapersonagens.dao.PersonagemDAO;
 import com.example.listapersonagens.model.Personagem;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import static com.example.listapersonagens.ui.activities.ConstantesActivities.CHAVE_PERSONAGEM;
 
@@ -26,12 +31,31 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     private Personagem personagem;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Metodo para utilizar o icone de check para salvar dados do personagem
+        getMenuInflater().inflate(R.menu.activity_formulario_personagem_menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Metodo para clicar no check e salvar os dados do personagem
+        int itemId = item.getItemId();
+        if(itemId == R.id.activity_formulario_personagem_menu_salvar) {
+            finalizaFormulario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_personagem);
-        
+        //Organização do formulario do personagem
         inicializacaoCampos();
+        //Metodo para utilizar o evento de click do botão para salvar
         configuraBotao();
+        //Metodo para construção do formulario
         carregaPersonagem();
 
     }
@@ -50,6 +74,7 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     }
 
     private void preencherCampos() {
+        //Metodo para associar as variaveis nome, altura e nascimento para serem setados
         nomePersonagem.setText(personagem.getNome());
         alturaPersonagem.setText(personagem.getAltura());
         nascimentoPersonagem.setText(personagem.getNascimento());
@@ -85,6 +110,17 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
 
         // Campo de nascimento do personagem (variavel)
         nascimentoPersonagem = findViewById(R.id.editTextDate);
+
+        //Criação de mascara para o campo altura
+        SimpleMaskFormatter smfAltura = new SimpleMaskFormatter("N,NN");
+        MaskTextWatcher mtwAltura = new MaskTextWatcher(alturaPersonagem, smfAltura);
+        alturaPersonagem.addTextChangedListener(mtwAltura);
+
+        //Criação de mascara para o campo nascimento
+        SimpleMaskFormatter smfNascimento = new SimpleMaskFormatter("NN/NN/NNNN");
+        MaskTextWatcher mtwNascimento = new MaskTextWatcher(nascimentoPersonagem, smfNascimento);
+        nascimentoPersonagem.addTextChangedListener(mtwNascimento);
+
     }
 
     private void preencherPersonagem() {
